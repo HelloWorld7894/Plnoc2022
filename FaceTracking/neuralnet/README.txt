57814a95c46929c44eb8f4e93c239262e01d14b5
HelloWorld7894 <danpojhan@protonmail.com>
2022-09-26 19:55:43 +0200

    Moved from ImageAi to mediapipe

--- FaceTracking/neuralnet/README.txt
+++
@@ -1,3 +0,0 @@
-NOTE: neuralnet directory uses ImageAi API, because ImageAi has some conflicts with 
-newer versions of some libraries (mostly tensorflow), i highly recomment creating virtual enviroment
-(all requirements are in requirements.txt file)
--- FaceTracking/neuralnet/main.py
+++ FaceTracking/neuralnet/main.py
@@ -1,18 +1,14 @@
-#Too lazy to implement model from tensorflow model garden,
-#used ImageAI TinyYOLOv3 model instead (https://github.com/OlafenwaMoses/ImageAI)
-
+#using SSD mobilenet from mediapipe API
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 import cv2
-from imageai.Detection import ObjectDetection
-import os
+import mediapipe as mp
 
-execution_path = os.getcwd()
+import cv2
+import mediapipe as mp
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
 
-detector = ObjectDetection()
-detector.setModelTypeAsTinyYOLOv3()
-detector.setModelPath( os.path.join(execution_path , "yolo-tiny.h5"))
-detector.loadModel()
 
 Cam = PiCamera()
 Cam.rotation = 180
@@ -20,12 +16,20 @@ Cam.resolution = (640, 480)
 Cam.framerate = 32
 
 RawCapture = PiRGBArray(Cam, size=(640, 480))
+face_detection = mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5)
 
 for frame in Cam.capture_continuous(RawCapture, format="bgr", use_video_port = True):
 
     Img = frame.array
     RawCapture.truncate(0)
 
-    Image, Detections = detector.detectObjectsFromImage(output_type="array", input_image=Img, input_type="array", minimum_percentage_probability=50)
-    cv2.imshow("Frame", Image)
-    cv2.waitKey(1) #Terminate by Ctrl+C in terminal
+    image = cv2.cvtColor(Img, cv2.COLOR_BGR2RGB)
+    results = face_detection.process(image)
+
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.detections:
+      for detection in results.detections:
+        mp_drawing.draw_detection(image, detection)
+    cv2.imshow('MediaPipe Face Detection', image)
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
